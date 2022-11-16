aa23bad8cac94cde81184d7b99a924e413b218fa
DESKTOP-U373PAQ\홍윤재 <hongpiglet1995@gmail.com>
2022-11-16 14:41:29 +0900

    bestseller.py - 불필요한 코드 삭제 및 크롬 드라이버 자동 업데이트 기능 삭제 newbooks.py -  불필요한 코드 삭제 및 크롬 드라이버 자동 업데이트 기능 삭제 / if문 활용하여 크롤링 갯수 제한 코드 작성 bestseller.css / newbooks.css - 마크업 크기 수정 및 줄바꿈 변화 기능 추가 bestseller.html/ newbooks.html - 선택된 네비게이션 메뉴 색상 수정

--- bestseller.py
+++ bestseller.py
@@ -1,59 +1,48 @@
-import json
-
 from pymongo import MongoClient
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-# Pageload Strategy 설정 변경
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import config
-import time
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 client = MongoClient(config.mongodb_url)
 db = client.bestseller
 
-url = "https://product.kyobobook.co.kr/bestseller/online?period=001"
+url = "https://product.kyobobook.co.kr/bestseller/online?period=002"
 
 caps = DesiredCapabilities().CHROME
 caps["pageLoadStrategy"] = "none"  # Pageload Strategy 설정 변경
 chrome_options = webdriver.ChromeOptions()  # 크롬 드라이버 실행
 chrome_options.add_argument("headless")  # 크롬 드라이버 창 감추기
-driver = webdriver.Chrome(service=Service(
-    ChromeDriverManager().install()), options=chrome_options)  # 크롬 드라이버 자동 업데이트
-# driver = webdriver.Chrome(options=chrome_options) #크롬 드라이버 업데이트 없이 실행
+driver = webdriver.Chrome(options=chrome_options) #크롬 드라이버 업데이트 없이 실행
 driver.get(url)
 
-previous = driver.execute_script("return document.body.scrollHeight")
-interval = 1
 
 def crawling_bestseller():
     soup = BeautifulSoup(driver.page_source, "html.parser")
     items = soup.find_all("li", attrs={"class": "prod_item"})
-    # time.sleep(3)
     extract = []
     try:
         for item in items:
 
             img = item.find("img")["src"]
-            print(img)
+            # print(img)
 
             title = item.find("span", attrs={"class": "prod_name"}).text
-            print(title)
+            # print(title)
 
             author = item.find("span", attrs={"class": "prod_author"}).text
-            print(author)
+            # print(author)
 
             price = item.find("span", attrs={"class": "val"}).text
-            print(price)
+            # print(price)
 
             desc = item.find("p", attrs={"class": "prod_introduction"}).text
-            print(desc)
+            # print(desc)
 
             star = item.find(
                 "span", attrs={"class": "review_klover_text font_size_xxs"}).text
-            print("*")
+            # print("*")
 
             doc = {
                 'title': title,
