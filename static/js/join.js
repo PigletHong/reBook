8d493f817ca474013a78d9479d324b3be262e33d
DESKTOP-U373PAQ\홍윤재 <hongpiglet1995@gmail.com>
2022-11-17 00:47:07 +0900

    1. ID, PW, 이름 정보 DB저장 기능 추가 2. input_box ID값 추가

---
+++ static/js/join.js
@@ -0,0 +1,19 @@
+function join() {
+    $.ajax({
+        type: "POST",
+        url: "/api/join",
+        data: {
+            id_give: $('#userId').val(),
+            pw_give: $('#userPwd').val(),
+            name_give: $('#userName').val()
+        },
+        success: function (response) {
+            if (response['result'] == 'success') {
+                alert('회원가입이 완료되었습니다.')
+                window.location.href = '/login'
+            } else {
+                alert(response['msg'])
+            }
+        }
+    })
+}
