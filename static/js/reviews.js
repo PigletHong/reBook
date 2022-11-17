b0f174bbb0acc17ef6b932eccf07642b51c072a6
Jdentity <identityjang@gmail.com>
2022-11-17 16:51:59 +0900

    review 검색 백엔드

--- static/js/reviews.js
+++ static/js/reviews.js
@@ -1,46 +1,71 @@
 // same with JQuery $(document).ready()
-document.addEventListener("DOMContentLoaded", function(event) { 
-  show_reviews();
+document.addEventListener("DOMContentLoaded", function(event) {
+  const params = new URLSearchParams(window.location.search);
+  const type = params.get('type');
+  const query = params.get('query');
+  // query가 있을 경우
+  if(type && query){
+    search_reviews(type, query);
+  }else {
+    // 없을 경우
+    get_reviews();
+  }
 });
 
-function show_reviews() {
+function get_reviews() {
   $.ajax({
     type: 'GET',
     url: '/api/review',
     data: {},
     success: function (response) {
-      const reviews = response['reviews'];
-
-      for(let i=0; i<reviews.length; i++){
-        const bookImg = reviews[i]['bookInfo']['img'];
-        const bookTitle = reviews[i]['bookInfo']['title'];
-        const id = reviews[i]['_id'];
-        const content = reviews[i]['content'];
-        const pubDate = reviews[i]['pubDate'];
-        const userId = "신원";
-        // const tag = reviews[i]['tag'];
-        // const userId = reviews[i]['userId'];
-        const temp_html = `
-          <div class="review-card" id="${id}">
-            <img
-              class="review-img"
-              src="${bookImg}"
-            />
-            <div class="text-area">
-              <h3 class="review-title">${bookTitle}</h3>
-              <p class="review-desc">
-                ${content}
-              </p>
-              <div class="detail-info">
-                <p class="review-createdBy">${pubDate}</p>
-                <p class="review-author">${userId}</p>
-              <div>
-            </div>
-          </div>
-        `
-        $('.reviews').append(temp_html);
-      }
+        show_reviews(response);
       }
     },
   );
+}
+
+function search_reviews(type, query) {
+  $.ajax({
+    type: 'GET',
+    url: `/api/review?type=${type}&query=${query}`,
+    data: {},
+    success: function (response) {
+        show_reviews(response);
+      }
+    },
+  );
+}
+
+function show_reviews(res) {
+  const reviews = res['reviews'];
+
+  for(let i=0; i<reviews.length; i++){
+    const bookImg = reviews[i]['bookInfo']['img'];
+    const bookTitle = reviews[i]['bookInfo']['title'];
+    const id = reviews[i]['_id'];
+    const content = reviews[i]['content'];
+    const pubDate = reviews[i]['pubDate'];
+    const userId = "신원";
+    // const tag = reviews[i]['tag'];
+    // const userId = reviews[i]['userId'];
+    const temp_html = `
+      <div class="review-card" id="${id}">
+        <img
+          class="review-img"
+          src="${bookImg}"
+        />
+        <div class="text-area">
+          <h3 class="review-title">${bookTitle}</h3>
+          <p class="review-desc">
+            ${content}
+          </p>
+          <div class="detail-info">
+            <p class="review-createdBy">${pubDate}</p>
+            <p class="review-author">${userId}</p>
+          <div>
+        </div>
+      </div>
+    `
+    $('.reviews').append(temp_html);
+  }
 }
